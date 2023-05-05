41c7a04cc21d547254d318149eb6a27d50dad228
abhi33333 <abhilasha.rana@gmail.com>
2023-05-05 18:44:34 -0400

    Added padding and margin on form

--- js/index.js
+++ js/index.js
@@ -7,13 +7,28 @@ $(document).ready(function(){
     $('.expandable').addClass('active');
 
     // Functionality for expanding and collapsing the form
-    $('.expandable, .collapsible').on('click', function(){
-      $('.form-content').slideToggle();
-      $('.expandable').toggleClass('active');
-      if($('.expandable').hasClass('active')) {
-        $('.expandable').text('Click here to collapse the form');
-      } else {
-        $('.expandable').text('Click here to expand the form');
-      }
-    });
+   // $('.expandable, .collapsible').on('click', function(){
+   //   $('.form-content').slideToggle();
+  //    $('.expandable').toggleClass('active');
+  //    if($('.expandable').hasClass('active')) {
+ //       $('.expandable').text('Click here to collapse the form');
+  //    } else {
+  //      $('.expandable').text('Click here to expand the form');
+ //     }
+ //   });
+
+
+ $('.expandable').on('click', function() {
+    if ($('.form-content').is(':visible')) {
+      $('.form-content').slideUp(function() {
+        $('.form-content label, .form-content input').hide();
+      });
+      $('.expandable').addClass('active').text('Click here to expand the form');
+    } else {
+      $('.form-content label, .form-content input').show();
+      $('.form-content').slideDown();
+      $('.expandable').removeClass('active').text('Click here to collapse the form');
+    }
   });
+
+});
